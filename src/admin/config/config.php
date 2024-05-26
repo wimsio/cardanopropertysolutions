b749a7aca3bebc7629b4fcd21a4bc506371ad7c1
Bernard Sibanda <dev@wims.io>
2024-05-26 13:36:39 +0200

    Add files via upload

---
+++ src/admin/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$con = mysqli_connect("localhost","cardanoproperty_dbMay20u","dbMay20u123!@#","cardanoproperty_dbMay20");
+	if (mysqli_connect_errno())
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+?>
