1df2de5465f8192bf87e1b5d8eb699b69e157f37
Bernard Sibanda <dev@wims.io>
2024-05-30 08:17:48 +0200

    deleted old admin files

--- src/admin/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost","cardanoproperty_dbMay20u","dbMay20u123!@#","cardanoproperty_dbMay20");
-	if (mysqli_connect_errno())
-	{
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
-?>
